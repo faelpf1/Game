b2ac436f9d510fcda20c898ad48db06e6bb01a18
Rafael <rafael.filgueiras@estudante.ifb.edu.br>
2023-05-21 10:48:03 +0000

    stairs

--- src/scenes/dungeonScene.js
+++ src/scenes/dungeonScene.js
@@ -13,6 +13,9 @@ export default class DungeonScene extends Phaser.Scene {
 	}
 
 	create() {
+		this.level++;
+		this.hasPlayerReachedStairs = false;
+
 		this.dungeon = new Dungeon({
 			width: 50,
 			height: 50,
@@ -23,74 +26,138 @@ export default class DungeonScene extends Phaser.Scene {
 			},
 		});
 
-		//this.dungeon.drawToConsole();
-
 		const map = this.make.tilemap({ tileWidth: 48, tileHeight: 48, width: this.dungeon.width, height: this.dungeon.height });
 		const tileset = map.addTilesetImage("tiles", null, 48, 48, 0, 0);  /* tile height and tile width, tile margin, tile spacing */
-		this.groundLayer = map.createBlankLayer("Ground", tileset).fill(TILES.BLANK);
-		this.wallLayer = map.createBlankLayer("Wall", tileset).fill(TILES.BLANK);
-		this.stuffLayer = map.createBlankLayer("Stuff", tileset);
+		this.groundLayer = map.createBlankLayer("Ground", tileset).fill(TILES.BLANK); /* Layer for floors */
+		this.wallLayer = map.createBlankLayer("Wall", tileset).fill(TILES.BLANK); /* Layer for walls */
+		this.stuffLayer = map.createBlankLayer("Stuff", tileset); /* Layer for stuffs or objects */
 
 		/* Generate tiles on map */
 		this.dungeon.rooms.forEach((room) => {
 			const { x, y, width, height, left, right, top, bottom } = room;
 
 			/* Place floor */
-			this.groundLayer.weightedRandomize(TILES.FLOOR, x , y+1 , width-2 , height-2 );
+			this.groundLayer.weightedRandomize(TILES.FLOOR, x, y + 1, width - 2, height - 2);
 
 			/* Place corner walls */
-			this.wallLayer.putTileAt(TILES.WALL.TOP_LEFT_UP, left , top-1);
-			this.wallLayer.putTileAt(TILES.WALL.TOP_LEFT_DOWN, left , top);
+			this.wallLayer.putTileAt(TILES.WALL.TOP_LEFT_UP, left, top - 1);
+			this.wallLayer.putTileAt(TILES.WALL.TOP_LEFT_DOWN, left, top);
 
-			this.wallLayer.putTileAt(TILES.WALL.TOP_RIGHT_UP, right-2, top-1)
-			this.wallLayer.putTileAt(TILES.WALL.TOP_RIGHT_DOWN, right-2, top)
+			this.wallLayer.putTileAt(TILES.WALL.TOP_RIGHT_UP, right - 2, top - 1)
+			this.wallLayer.putTileAt(TILES.WALL.TOP_RIGHT_DOWN, right - 2, top)
 
-			this.wallLayer.putTileAt(TILES.WALL.BOTTOM_LEFT_UP, left, bottom-2);
-			this.wallLayer.putTileAt(TILES.WALL.BOTTOM_LEFT_DOWN, left, bottom-1);
+			this.wallLayer.putTileAt(TILES.WALL.BOTTOM_LEFT_UP, left, bottom - 2);
+			this.wallLayer.putTileAt(TILES.WALL.BOTTOM_LEFT_DOWN, left, bottom - 1);
 
-			this.wallLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT_UP, right-2, bottom-2);
-			this.wallLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT_DOWN, right-2, bottom-1);
-			
+			this.wallLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT_UP, right - 2, bottom - 2);
+			this.wallLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT_DOWN, right - 2, bottom - 1);
+
 			/* Fill the walls with mostly clean tiles */
-			this.wallLayer.weightedRandomize(TILES.WALL.TOP_UP, left+1, top-1 , width-4, 1);
-			this.wallLayer.weightedRandomize(TILES.WALL.TOP_DOWN, left+1, top, width-4, 1);
-			this.wallLayer.weightedRandomize(TILES.WALL.BOTTOM_UP, left+1, bottom-2, width-4, 1);
-			this.wallLayer.weightedRandomize(TILES.WALL.BOTTOM_DOWN, left+1, bottom-1, width-4, 1);
-			this.wallLayer.weightedRandomize(TILES.WALL.LEFT, left, top+1 , 1, height-4);
-			this.wallLayer.weightedRandomize(TILES.WALL.RIGHT, right-2, top+1 , 1, height-4);
+			this.wallLayer.weightedRandomize(TILES.WALL.TOP_UP, left + 1, top - 1, width - 4, 1);
+			this.wallLayer.weightedRandomize(TILES.WALL.TOP_DOWN, left + 1, top, width - 4, 1);
+			this.wallLayer.weightedRandomize(TILES.WALL.BOTTOM_UP, left + 1, bottom - 2, width - 4, 1);
+			this.wallLayer.weightedRandomize(TILES.WALL.BOTTOM_DOWN, left + 1, bottom - 1, width - 4, 1);
+			this.wallLayer.weightedRandomize(TILES.WALL.LEFT, left, top + 1, 1, height - 4);
+			this.wallLayer.weightedRandomize(TILES.WALL.RIGHT, right - 2, top + 1, 1, height - 4);
 
 			// Dungeons have rooms that are connected with doors. Each door has an x & y relative to the
 			// room's location. Each direction has a different door to tile mapping.
-			//const doors = room.getDoorLocations(); // → Returns an array of {x, y} objects
-			// for (let i = 0; i < doors.length; i++) {
-			// 	if (doors[i].y === 0) {
-			// 		this.groundLayer.putTilesAt(TILES.DOOR.TOP, x + doors[i].x - 1, y + doors[i].y);
-			// 	} else if (doors[i].y === room.height - 1) {
-			// 		this.groundLayer.putTilesAt(TILES.DOOR.BOTTOM, x + doors[i].x - 1, y + doors[i].y);
-			// 	} else if (doors[i].x === 0) {
-			// 		this.groundLayer.putTilesAt(TILES.DOOR.LEFT, x + doors[i].x, y + doors[i].y - 1);
-			// 	} else if (doors[i].x === room.width - 1) {
-			// 		this.groundLayer.putTilesAt(TILES.DOOR.RIGHT, x + doors[i].x, y + doors[i].y - 1);
-			// 	}
-			// }
+			const doors = room.getDoorLocations(); // → Returns an array of {x, y} objects
+			for (let i = 0; i < doors.length; i++) {
+				if (doors[i].y === 0) {
+					//this.wallLayer.putTilesAt(TILES.DOOR.TOP, x + doors[i].x - 1, y + doors[i].y);
+				} else if (doors[i].y === room.height - 1) {
+					//this.groundLayer.putTilesAt(TILES.DOOR.BOTTOM, x + doors[i].x - 1, y + doors[i].y);
+				} else if (doors[i].x === 0) {
+					//this.groundLayer.putTilesAt(TILES.DOOR.LEFT, x + doors[i].x, y + doors[i].y - 1);
+				} else if (doors[i].x === room.width - 1) {
+					//this.groundLayer.putTilesAt(TILES.DOOR.RIGHT, x + doors[i].x, y + doors[i].y - 1);
+				}
+			}
 		});
 
-		this.groundLayer.setCollisionByExclusion([-1, 6, 7, 8, 26]); /* Not exactly correct for the tileset since there are more possible floor tiles, but this will do for the example. */
+		const rooms = this.dungeon.rooms.slice();
+		const startRoom = rooms.shift();
+		const endRoom = Phaser.Utils.Array.RemoveRandomElement(rooms);
+		const otherRooms = Phaser.Utils.Array.Shuffle(rooms).slice(0, rooms.length * 0.9);
 
-		this.player = new Player(this, map.widthInPixels / 2, map.heightInPixels / 2); /* Place the player in the center of the map */
+		// Place the stairs
+		this.stuffLayer.putTileAt(TILES.STAIRS, endRoom.centerX, endRoom.centerY);
 
-		this.physics.add.collider(this.player.sprite, this.groundLayer); /* Watch the player and ground layer for collisions, for the duration of the scene: */
+		otherRooms.forEach((room) => {
+			const rand = Math.random();
+			if (rand <= 0.25) {
+				// 25% chance of chest
+				this.stuffLayer.putTileAt(TILES.CHEST, room.centerX, room.centerY);
+			} else if (rand <= 0.5) {
+				// 50% chance of a pot anywhere in the room... except don't block a door!
+				const x = Phaser.Math.Between(room.left + 2, room.right - 2);
+				const y = Phaser.Math.Between(room.top + 2, room.bottom - 2);
+				this.stuffLayer.weightedRandomize(x, y, 1, 1, TILES.POT);
+			} else {
+				// 25% of either 2 or 4 towers, depending on the room size
+				if (room.height >= 9) {
+					this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY + 1);
+					this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY + 1);
+					this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY - 2);
+					this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY - 2);
+				} else {
+					this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY - 1);
+					this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY - 1);
+				}
+			}
+		});
 
-		const camera = this.cameras.main; /* Acess default phaer default camera */ 
 
-		camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels); /* Constrain the camera so that it isn't allowed to move outside the width/height of tilemap */
+		/* Collisions */
+		this.groundLayer.setCollisionByExclusion([-1, 185, 188]);
+		this.wallLayer.setCollisionByExclusion([-1, 185, 188]);
+		this.stuffLayer.setCollisionByExclusion([-1, 185, 188]);
+
+		this.stuffLayer.setTileIndexCallback(TILES.STAIRS, () => {
+			this.stuffLayer.setTileIndexCallback(TILES.STAIRS, null);
+			this.hasPlayerReachedStairs = true;
+			this.player.freeze();
+			const cam = this.cameras.main;
+			cam.fade(250, 0, 0, 0);
+			cam.once("camerafadeoutcomplete", () => {
+				this.player.destroy();
+				this.scene.restart();
+			});
+		});
+
+		// Place the player in the first room
+		const playerRoom = startRoom;
+		const x = map.tileToWorldX(playerRoom.centerX);
+		const y = map.tileToWorldY(playerRoom.centerY);
+		this.player = new Player(this, x, y);
+
+		/* Collision */
+		//this.physics.add.collider(this.player.sprite, this.groundLayer);
+		//this.physics.add.collider(this.player.sprite, this.wallLayer);
+		this.physics.add.collider(this.player.sprite, this.stuffLayer);
+
+		/* Phaser default camera */
+		const camera = this.cameras.main;
+
+		// Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
+		camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 		camera.startFollow(this.player.sprite);
 
-		/* Help text that has a "fixed" position on the screen */
-		this.add.text(16, 16, `Find the stairs. Go deeper.\nCurrent level: ${this.level}`, { font: "18px monospace", fill: "#000000", padding: { x: 20, y: 10 }, backgroundColor: "#ffffff"}).setScrollFactor(0);
+		this.add.text(16, 16, `Current level: ${this.level}`, { font: "18px monospace", fill: "#000000", padding: { x: 20, y: 10 }, backgroundColor: "#ffffff" }).setScrollFactor(0);
 	}
 
 	update(time, delta) {
+		if (this.hasPlayerReachedStairs) return;
+
 		this.player.update();
+
+		// Find the player's room using another helper method from the dungeon that converts from
+		// dungeon XY (in grid units) to the corresponding room object
+		//const playerTileX = this.groundLayer.worldToTileX(this.player.sprite.x);
+		//const playerTileY = this.groundLayer.worldToTileY(this.player.sprite.y);
+		//const playerRoom = this.dungeon.getRoomAt(playerTileX, playerTileY);
+
+		//this.tilemapVisibility.setActiveRoom(playerRoom);
 	}
 }
