672f3e7678b83c55a4433b520ca46366b3eb43ae
Rafael <rafael.filgueiras@estudante.ifb.edu.br>
2023-06-09 18:58:16 +0000

    refactoring

--- src/scenes/dungeonScene/dungeonScene.js
+++ src/scenes/dungeonScene/dungeonScene.js
@@ -4,7 +4,8 @@ import Skeleton from '../../enemies/skeleton.js';
 import Skull from '../../enemies/skull.js';
 import Text from '../../components/text.js';
 import cameraConfig from './cameraConfig.js';
-import playerConfig from './playerConfig.js'
+import playerConfig from './playerConfig.js';
+import generateMap from './generateMap.js';
 
 export default class DungeonScene extends Phaser.Scene {
     constructor() {
@@ -16,12 +17,13 @@ export default class DungeonScene extends Phaser.Scene {
         this.level++;
         
         this.hasPlayerReachedStairs = false;
-        this.registry.set('stageInfo', this.level);
+        this.registry.set('stageInfo', this.level); /* Pass current level to levelinfo scene */ 
         
         this.dungeonConfig();
         const map = this.make.tilemap({ tileWidth: 48, tileHeight: 48, width: this.dungeon.width, height: this.dungeon.height });
-        this.generateTilesMap(map); /* Generate tiles on map */
-
+        
+        this.mapConfig(map);
+        
         const rooms = this.dungeon.rooms.slice();
         const startRoom = rooms.shift();
         const endRoom = Phaser.Utils.Array.RemoveRandomElement(rooms);
@@ -64,69 +66,13 @@ export default class DungeonScene extends Phaser.Scene {
         });
     }
 
-    generateTilesMap(map) {
+    mapConfig(map) {
         const tileset = map.addTilesetImage("tiles", null, 48, 48, 0, 0);  /* tile height and tile width, tile margin, tile spacing */
         this.groundLayer = map.createBlankLayer("Ground", tileset).fill(TILES.BLANK); /* Layer for floors */
         this.wallLayer = map.createBlankLayer("Wall", tileset).fill(TILES.BLANK); /* Layer for walls */
         this.stuffLayer = map.createBlankLayer("Stuff", tileset); /* Layer for stuffs or objects */
-
-        this.dungeon.rooms.forEach((room) => {
-            const { x, y, width, height, left, right, top, bottom } = room;
-
-            /* Place floor */
-            this.groundLayer.weightedRandomize(TILES.FLOOR, x, y + 1, width - 2, height - 2);
-
-            /* Place corner walls */
-            this.wallLayer.putTileAt(TILES.WALL.TOP_LEFT_UP, left, top - 1);
-            this.wallLayer.putTileAt(TILES.WALL.TOP_LEFT_DOWN, left, top);
-
-            this.wallLayer.putTileAt(TILES.WALL.TOP_RIGHT_UP, right - 2, top - 1)
-            this.wallLayer.putTileAt(TILES.WALL.TOP_RIGHT_DOWN, right - 2, top)
-
-            this.wallLayer.putTileAt(TILES.WALL.BOTTOM_LEFT_UP, left, bottom - 2);
-            this.wallLayer.putTileAt(TILES.WALL.BOTTOM_LEFT_DOWN, left, bottom - 1);
-
-            this.wallLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT_UP, right - 2, bottom - 2);
-            this.wallLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT_DOWN, right - 2, bottom - 1);
-
-            /* Fill the walls with mostly clean tiles */
-            this.wallLayer.weightedRandomize(TILES.WALL.TOP_UP, left + 1, top - 1, width - 4, 1);
-            this.wallLayer.weightedRandomize(TILES.WALL.TOP_DOWN, left + 1, top, width - 4, 1);
-            this.wallLayer.weightedRandomize(TILES.WALL.BOTTOM_UP, left + 1, bottom - 2, width - 4, 1);
-            this.wallLayer.weightedRandomize(TILES.WALL.BOTTOM_DOWN, left + 1, bottom - 1, width - 4, 1);
-            this.wallLayer.weightedRandomize(TILES.WALL.LEFT, left, top + 1, 1, height - 4);
-            this.wallLayer.weightedRandomize(TILES.WALL.RIGHT, right - 2, top + 1, 1, height - 4);
-
-            // Dungeons have rooms that are connected with doors. Each door has an x & y relative to the
-            // room's location. Each direction has a different door to tile mapping.
-            const doors = room.getDoorLocations(); // → Returns an array of {x, y} objects
-            for (let i = 0; i < doors.length; i++) {
-                if (doors[i].y === 0) {
-                    this.wallLayer.putTilesAt(TILES.DOOR.TOP_UP, x + doors[i].x - 2, y + doors[i].y - 1);
-                    this.wallLayer.putTilesAt(TILES.DOOR.TOP_DOWN, x + doors[i].x - 2, y + doors[i].y);
-                } else if (doors[i].y === room.height - 1) {
-                    this.wallLayer.putTilesAt(TILES.DOOR.BOTTOM_UP, x + doors[i].x - 2, y + doors[i].y - 2);
-                    this.wallLayer.putTilesAt(TILES.DOOR.BOTTOM_DOWN, x + doors[i].x - 2, y + doors[i].y - 1);
-                } else if (doors[i].x === 0) {
-                    this.wallLayer.putTilesAt(TILES.DOOR.LEFT_UP, x + doors[i].x, y + doors[i].y - 1);
-                    this.groundLayer.putTilesAt(TILES.DOOR.FLOOR, x + doors[i].x - 1, y + doors[i].y + 1);
-                    this.wallLayer.putTilesAt(TILES.DOOR.LEFT_DOWN, x + doors[i].x - 1, y + doors[i].y - 1);
-                } else if (doors[i].x === room.width - 1) {
-                    this.wallLayer.putTilesAt(TILES.DOOR.RIGHT_UP, x + doors[i].x - 2, y + doors[i].y - 1);
-                    this.groundLayer.putTilesAt(TILES.DOOR.FLOOR, x + doors[i].x - 1, y + doors[i].y + 1);
-                    this.wallLayer.putTilesAt(TILES.DOOR.RIGHT_DOWN, x + doors[i].x - 1, y + doors[i].y - 1);
-                }
-            }
-        });
-        this.layerCollission(); /* Layers collisions */
-    }
-
-    layerCollission() {
-        const collisionArray = [-1, 18, 16, 0, 117, 185, 188]
-        this.groundLayer.setCollisionByExclusion(collisionArray);
-        this.wallLayer.setCollisionByExclusion(collisionArray);
-        this.stuffLayer.setCollisionByExclusion(collisionArray);
-        this.wallLayer.setCollisionByProperty({ collides: true });
+        generateMap(this.dungeon, map, tileset, this.groundLayer, this.wallLayer/*, this.stuffLayer*/);
+        //({stuffLayer: this.stuffLayer} = generateMap(this.dungeon, map, tileset, this.groundLayer, this.wallLayer));
     }
 
     dungeonStageChange(endRoom) {
@@ -145,32 +91,32 @@ export default class DungeonScene extends Phaser.Scene {
         });
     }
 
-    generateStuffs(rooms) {
-        const otherRooms = Phaser.Utils.Array.Shuffle(rooms).slice(0, rooms.length * 0.9);
-        otherRooms.forEach((room) => {
-            const rand = Math.random();
-            if (rand <= 0.25) {
-                // 25% chance of chest
-                this.stuffLayer.putTileAt(TILES.CHEST, room.centerX, room.centerY);
-            } else if (rand <= 0.5) {
-                // 50% chance of a pot anywhere in the room... except don't block a door!
-                const x = Phaser.Math.Between(room.left + 2, room.right - 2);
-                const y = Phaser.Math.Between(room.top + 2, room.bottom - 2);
-                this.stuffLayer.weightedRandomize(x, y, 1, 1, TILES.POT);
-            } else {
-                // 25% of either 2 or 4 towers, depending on the room size
-                if (room.height >= 9) {
-                    this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY + 1);
-                    this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY + 1);
-                    this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY - 2);
-                    this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY - 2);
-                } else {
-                    this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY - 1);
-                    this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY - 1);
-                }
-            }
-        });
-    }
+    // generateStuffs(rooms) {
+    //     const otherRooms = Phaser.Utils.Array.Shuffle(rooms).slice(0, rooms.length * 0.9);
+    //     otherRooms.forEach((room) => {
+    //         const rand = Math.random();
+    //         if (rand <= 0.25) {
+    //             // 25% chance of chest
+    //             this.stuffLayer.putTileAt(TILES.CHEST, room.centerX, room.centerY);
+    //         } else if (rand <= 0.5) {
+    //             // 50% chance of a pot anywhere in the room... except don't block a door!
+    //             const x = Phaser.Math.Between(room.left + 2, room.right - 2);
+    //             const y = Phaser.Math.Between(room.top + 2, room.bottom - 2);
+    //             this.stuffLayer.weightedRandomize(x, y, 1, 1, TILES.POT);
+    //         } else {
+    //             // 25% of either 2 or 4 towers, depending on the room size
+    //             if (room.height >= 9) {
+    //                 this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY + 1);
+    //                 this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY + 1);
+    //                 this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY - 2);
+    //                 this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY - 2);
+    //             } else {
+    //                 this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY - 1);
+    //                 this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY - 1);
+    //             }
+    //         }
+    //     });
+    // }
 
 
     /* Player Configs */
---
+++ src/scenes/dungeonScene/generateMap.js
@@ -0,0 +1,75 @@
+import TILES from './tile-mapping.js';
+
+export default function generateMap(dungeon, map, tileset, groundLayer, wallLayer) {  
+    // this.groundLayer = map.createBlankLayer("Ground", tileset).fill(TILES.BLANK); /* Layer for floors */
+    // this.wallLayer = map.createBlankLayer("Wall", tileset).fill(TILES.BLANK); /* Layer for walls */
+    //this.stuffLayer = map.createBlankLayer("Stuff", tileset); /* Layer for stuffs or objects */
+
+    dungeon.rooms.forEach((room) => {
+        const { x, y, width, height, left, right, top, bottom } = room;
+        placeFloor(groundLayer, x, y, width, height);
+        placeCornerWall(wallLayer, left, right, top, bottom);
+        placeMiddleWall(wallLayer, left, right, top, bottom, width, height);
+        placePathBetweenRoom(room, wallLayer, groundLayer, x, y);
+        layerCollission(groundLayer, wallLayer);
+    });
+    //return {/*groundLayer, wallLayer,*/ stuffLayer};
+}
+
+function placeFloor(groundLayer, x, y, width, height){
+    groundLayer.weightedRandomize(TILES.FLOOR, x, y + 1, width - 2, height - 2);
+}
+
+function placeCornerWall(wallLayer, left, right, top, bottom){
+    wallLayer.putTileAt(TILES.WALL.TOP_LEFT_UP, left, top - 1);
+    wallLayer.putTileAt(TILES.WALL.TOP_LEFT_DOWN, left, top);
+
+    wallLayer.putTileAt(TILES.WALL.TOP_RIGHT_UP, right - 2, top - 1);
+    wallLayer.putTileAt(TILES.WALL.TOP_RIGHT_DOWN, right - 2, top);
+
+    wallLayer.putTileAt(TILES.WALL.BOTTOM_LEFT_UP, left, bottom - 2);
+    wallLayer.putTileAt(TILES.WALL.BOTTOM_LEFT_DOWN, left, bottom - 1);
+
+    wallLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT_UP, right - 2, bottom - 2);
+    wallLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT_DOWN, right - 2, bottom - 1);
+}
+
+function placeMiddleWall(wallLayer, left, right, top, bottom, width, height){
+    wallLayer.weightedRandomize(TILES.WALL.TOP_UP, left + 1, top - 1, width - 4, 1);
+    wallLayer.weightedRandomize(TILES.WALL.TOP_DOWN, left + 1, top, width - 4, 1);
+    wallLayer.weightedRandomize(TILES.WALL.BOTTOM_UP, left + 1, bottom - 2, width - 4, 1);
+    wallLayer.weightedRandomize(TILES.WALL.BOTTOM_DOWN, left + 1, bottom - 1, width - 4, 1);
+    wallLayer.weightedRandomize(TILES.WALL.LEFT, left, top + 1, 1, height - 4);
+    wallLayer.weightedRandomize(TILES.WALL.RIGHT, right - 2, top + 1, 1, height - 4);
+}
+
+function placePathBetweenRoom(room, wallLayer, groundLayer, x, y){
+    /* Dungeons have rooms that are connected with doors. Each door has an x & y relative to the
+        room's location. Each direction has a different door to tile mapping.*/
+    const doors = room.getDoorLocations();
+    for (let i = 0; i < doors.length; i++) {
+        if (doors[i].y === 0) {
+            wallLayer.putTilesAt(TILES.DOOR.TOP_UP, x + doors[i].x - 2, y + doors[i].y - 1);
+            wallLayer.putTilesAt(TILES.DOOR.TOP_DOWN, x + doors[i].x - 2, y + doors[i].y);
+        } else if (doors[i].y === room.height - 1) {
+            wallLayer.putTilesAt(TILES.DOOR.BOTTOM_UP, x + doors[i].x - 2, y + doors[i].y - 2);
+            wallLayer.putTilesAt(TILES.DOOR.BOTTOM_DOWN, x + doors[i].x - 2, y + doors[i].y - 1);
+        } else if (doors[i].x === 0) {
+            wallLayer.putTilesAt(TILES.DOOR.LEFT_UP, x + doors[i].x, y + doors[i].y - 1);
+            groundLayer.putTilesAt(TILES.DOOR.FLOOR, x + doors[i].x - 1, y + doors[i].y + 1);
+            wallLayer.putTilesAt(TILES.DOOR.LEFT_DOWN, x + doors[i].x - 1, y + doors[i].y - 1);
+        } else if (doors[i].x === room.width - 1) {
+            wallLayer.putTilesAt(TILES.DOOR.RIGHT_UP, x + doors[i].x - 2, y + doors[i].y - 1);
+            groundLayer.putTilesAt(TILES.DOOR.FLOOR, x + doors[i].x - 1, y + doors[i].y + 1);
+            wallLayer.putTilesAt(TILES.DOOR.RIGHT_DOWN, x + doors[i].x - 1, y + doors[i].y - 1);
+        }
+    }
+}
+
+function layerCollission(groundLayer, wallLayer){
+    const collisionArray = [-1, 18, 16, 0, 117, 185, 188];
+    groundLayer.setCollisionByExclusion(collisionArray);
+    wallLayer.setCollisionByExclusion(collisionArray);
+    //stuffLayer.setCollisionByExclusion(collisionArray);
+    wallLayer.setCollisionByProperty({ collides: true });
+}
