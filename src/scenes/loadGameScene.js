240d85060cddc7b479e91e65578f32f44081a0fe
Rafael <rafael.filgueiras@estudante.ifb.edu.br>
2023-06-10 16:16:32 +0000

    fixed: first level number not visible

--- src/scenes/loadGameScene.js
+++ src/scenes/loadGameScene.js
@@ -10,6 +10,7 @@ export default class LoadGameScene extends Phaser.Scene
         this.load.image('tiles', './assets/tileMap/tileMapDungeon.png');
         this.load.spritesheet('characters', './assets/player/mainCharacter.png', { frameWidth: 16, frameHeight: 16, margin: 0, spacing: 0 });
         this.load.spritesheet('enemies','assets/enemy/teste.png', { frameWidth: 16, frameHeight: 16, margin: 0, spacing: 0 } );
-        this.load.on('complete', ()=>{ this.scene.start('DungeonScene'), this.scene.start('LevelInfo') });
+        this.load.on('complete', ()=>{ this.scene.start('DungeonScene'), this.scene.start('StageInfoScene');});
     }
 }
+
