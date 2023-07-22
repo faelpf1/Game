59e5d9f52fb0519503773e7ce91d47bcd973f612
Rafael de Paula Filgueiras <51681402+faelpf1@users.noreply.github.com>
2023-07-22 22:27:06 +0000

    create temp in menu testing battle scene

--- src/scenes/loadGameScene.js
+++ src/scenes/loadGameScene.js
@@ -9,8 +9,10 @@ export default class LoadGameScene extends Phaser.Scene
 
     preload() 
     {
+        this.load.image('gameLogo', './assets/mainMenu/dungeon.png');
         this.load.image('startGame', './assets/mainMenu/start.png');
         this.load.image('loadGame', './assets/mainMenu/load.png');
+        this.load.image('test', './assets/mainMenu/testing.png');
         this.load.image('selectionArrow', './assets/mainMenu/selectionArrow.png');
         this.load.image('tiles', './assets/tileMap/tileMapDungeon.png');
         this.load.spritesheet('characters', './assets/player/mainCharacter.png', { frameWidth: 16, frameHeight: 16, margin: 0, spacing: 0 });
@@ -20,11 +22,12 @@ export default class LoadGameScene extends Phaser.Scene
 
     create()
     {
+        let logo = this.add.image(this.game.renderer.width/2 , this.game.renderer.height/2 - 150, 'gameLogo');
         let selectionButton = this.add.sprite(-1, -1, 'selectionArrow');
         selectionButton.setVisible(false);
-        let startButton = this.add.image(this.game.renderer.width /2, this.game.renderer.height/2, 'startGame').setDepth(1);
-        let loadButton = this.add.image(this.game.renderer.width /2, this.game.renderer.height/2+100, 'loadGame').setDepth(1);
-  
+        let startButton = this.add.image(this.game.renderer.width/2, this.game.renderer.height/2, 'startGame').setDepth(1);
+        let loadButton = this.add.image(this.game.renderer.width/2, this.game.renderer.height/2 + 100, 'loadGame').setDepth(1);
+        
         startButton.setInteractive();
         startButton.on('pointerover', ()=>{
             selectionButton.setVisible(true);
@@ -41,7 +44,7 @@ export default class LoadGameScene extends Phaser.Scene
         });
 
 
-        loadButton.setInteractive();  //testando battlescene
+        loadButton.setInteractive();  
         loadButton.on('pointerover', ()=>{
             selectionButton.setVisible(true);
             selectionButton.x = loadButton.x - loadButton.width;
@@ -51,6 +54,21 @@ export default class LoadGameScene extends Phaser.Scene
             selectionButton.setVisible(false);
         });
         loadButton.on('pointerdown', ()=>{
+            //this.scene.start('BattleScene');
+        });
+
+        /* testando battlescene */
+        let testButton = this.add.image(this.game.renderer.width/2, this.game.renderer.height/2 + 200, 'test').setDepth(1);
+        testButton.setInteractive(); 
+        testButton.on('pointerover', ()=>{
+            selectionButton.setVisible(true);
+            selectionButton.x = testButton.x - testButton.width;
+            selectionButton.y = testButton.y;
+        });
+        testButton.on('pointerout', ()=>{
+            selectionButton.setVisible(false);
+        });
+        testButton.on('pointerdown', ()=>{
             this.scene.start('BattleScene');
         });
         
